0022cc1e0d51ada6116e022c9d2742f43f727861
Nickolay <kolagrekalo@gmail.com>
2021-01-29 19:47:35 +0200

    finished api's

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,82 +4,67 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Support\Carbon;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return Task::orderBy("created_at", "DESC")->get();
+
+  public function index()
+  {
+      return Task::orderBy("created_at", "DESC")->get();
+  }
+
+
+  public function create()
+  {
+      //
+  }
+
+  public function store(Request $request)
+  {
+    $new_task = new Task();
+    $new_task->description = $request->task["description"];
+    $new_task->save();
+
+    return $new_task;
+  }
+
+
+  public function show($id)
+  {
+
+  }
+
+
+  public function edit($id)
+  {
+
+
+  }
+
+
+  public function update(Request $request, $id)
+  {
+    $existing_task = Task::find($id);
+
+    if ($existing_task) {
+      $existing_task->completed = $request->task["completed"] ? true : false;
+      $existing_task->completed_at = $request->task["completed"] ? Carbon::now() : null;
+      $existing_task->save();
+
+      return $existing_task;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    return "Task not found.";
+  }
+
+  public function destroy($id) {
+    $existing_task = Task::find($id);
+    if ($existing_task) {
+      $existing_task->delete();
+      return "Task successfully deleted";
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    return "Task not found.";
+  }
 }
